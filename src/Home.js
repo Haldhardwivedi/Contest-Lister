f2694cbfb62f3901b199fd3141e4e30c9cbf2486
Haldhardwivedi <haldhardwivedi@gmail.com>
2022-01-18 16:19:21 +0530

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import './Home.css';
+import React from 'react';
+
+function Home() {
+    return (
+     <>
+     <div className='row '>
+       <p className='home col-sm-4'>
+         Hey Welcome to contest Lister
+         Click on The different Link in the Nav Bar to List all Finished 
+         Contest of different Divisions on Codeforces
+         Happy Coding !!. 
+       </p>
+     </div>
+     </>
+    );
+  }
+  
+  export default Home;
+  
