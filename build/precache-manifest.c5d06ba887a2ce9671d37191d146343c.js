67fa24b9c7703f3bfdad657e9c96e63360d04f59
Haldhardwivedi <haldhardwivedi@gmail.com>
2022-01-18 16:18:42 +0530

    Add files via upload

---
+++ build/precache-manifest.c5d06ba887a2ce9671d37191d146343c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fa551588fc57f32206bd7a8db91996f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874a554a15ee8581f01c",
+    "url": "/static/css/main.04d038d3.chunk.css"
+  },
+  {
+    "revision": "f61eb5b5a98055e39ba7",
+    "url": "/static/js/2.1c5bb241.chunk.js"
+  },
+  {
+    "revision": "b69dfa28a9772076cbe7",
+    "url": "/static/js/3.34dad17e.chunk.js"
+  },
+  {
+    "revision": "874a554a15ee8581f01c",
+    "url": "/static/js/main.a852ab27.chunk.js"
+  },
+  {
+    "revision": "46d71f08999d1046421b",
+    "url": "/static/js/runtime~main.59dd824d.js"
+  }
+]);
