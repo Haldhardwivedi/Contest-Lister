b154ef5bd48588a81b02f46845c53030a1b81d2e
Haldhardwivedi <haldhardwivedi@gmail.com>
2022-01-18 14:57:23 +0530

    Add files via upload

---
+++ src/Div2.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Navbar from "./Navbar"
+
+let link  = 'http://codeforces.com/contest/';
+
+function Div2() {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() =>  {
+    const url = "https://codeforces.com/api/contest.list";
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json["result"]);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  });
+  
+  return (
+    <>
+     <Navbar />
+     <div>
+        <table className='table table-dark'>
+          <thead>
+            <tr>
+              <td>Contest Name</td>
+              <td>Contest Link</td>
+            </tr>
+          </thead>
+          <tbody>
+          {data.map((contests) => (
+            <tr key={contests.id}>
+              {contests.phase==="FINISHED" &&(contests.name.indexOf("Div. 2")!==-1)&&
+                <>
+                  <td key={contests.id}>{contests.name}</td>
+                  <td><a className="link-light" href={link + contests.id}>Link</a></td>
+                </>
+              }
+            </tr>
+          ))}
+        </tbody>
+        </table>
+    </div>
+    </>
+  );
+}
+
+export default Div2;
